5be393ceb07de27c7aa45438b1cdd5c665256e4d
lllMaxMaxlll <max.herr.88@gmail.com>
2023-02-11 17:48:43 -0300

    Create genre controllers

---
+++ api/src/routes/controllers/genresControllers.js
@@ -0,0 +1,16 @@
+const { Genre } = require("../../db");
+const getApiGenres = require("./getGenresFN");
+
+const getGenres = async () => {
+	// All genres from API
+	const apiGenres = await getApiGenres();
+	// Add to DB
+	const genreToDB = await Genre.bulkCreate(
+		await apiGenres.map((G) => ({
+			name: G.name,
+		}))
+	);
+	return genreToDB;
+};
+
+module.exports = { getGenres };
