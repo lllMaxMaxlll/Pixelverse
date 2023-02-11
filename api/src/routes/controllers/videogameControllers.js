e2a5f495a5e952f693cb3e76ae53ee8a83d51d08
lllMaxMaxlll <max.herr.88@gmail.com>
2023-02-11 17:50:39 -0300

    Create find ID FN

--- api/src/routes/controllers/videogameControllers.js
+++ api/src/routes/controllers/videogameControllers.js
@@ -1,9 +1,23 @@
-/*
-getUsersById = async (id, source) => {
-  const user = source === 'bdd' ?
-    await Videogame.findByPk(id) :
-    await (axios.get(`{URl}/game/${id}`)).data;
+require("dotenv").config();
+const axios = require("axios");
+const { URL, API_KEY } = process.env;
+const { Videogame } = require("../../db");
 
-  return user
-}
- */
+// Find game with ID in DB
+const findID_DB = async (id) => {
+	const game = await Videogame.findByPk(id);
+	if (!game) throw Error("Game not found in Database");
+	return game;
+};
+
+// Find game with ID in API
+const findID_API = async (id) => {
+	try {
+		const game = await axios.get(`${URL}/games/${id}?key=${API_KEY}`);
+		return game.data;
+	} catch (error) {
+		throw Error("No se encontro en la API");
+	}
+};
+
+module.exports = { findID_API, findID_DB };
