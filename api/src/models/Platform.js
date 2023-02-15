9f31975eed5160d1fba83d84af6227c925777021
lllMaxMaxlll <max.herr.88@gmail.com>
2023-02-15 20:18:13 -0300

    Fix id model to integer

--- api/src/models/Platform.js
+++ api/src/models/Platform.js
@@ -5,8 +5,8 @@ module.exports = (sequelize) => {
 		"platform",
 		{
 			id: {
-				type: DataTypes.UUID,
-				defaultValue: DataTypes.UUIDV4,
+				type: DataTypes.INTEGER,
+				autoIncrement: true,
 				primaryKey: true,
 			},
 			name: {
