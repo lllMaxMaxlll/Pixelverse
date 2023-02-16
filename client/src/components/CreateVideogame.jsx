3f95585c3f50b134f9dc76c8c4d37926e1b4ddad
lllMaxMaxlll <max.herr.88@gmail.com>
2023-02-16 09:34:27 -0300

    Change icon, relocated components

--- client/src/components/CreateVideogame.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const CreateVideogame = () => {
-	return <div>CreateVideogame</div>;
-};
-export default CreateVideogame;
--- client/src/components/VideogameCard.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const VideogameCard = () => {
-	return <div>VideogameCard</div>;
-};
-export default VideogameCard;
