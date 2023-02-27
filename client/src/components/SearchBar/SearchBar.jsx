b0f08b0442a4a478698c32ecc3988e5dc2044f9a
lllMaxMaxlll <max.herr.88@gmail.com>
2023-02-27 10:42:52 -0300

    Add style input/buttons search

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { showAllGames, getVideogamesByName } from "../../redux/actions";
+import style from "./SearchBar.module.css";
 
 const SearchBar = () => {
 	const [text, setText] = useState("");
@@ -22,26 +23,35 @@ const SearchBar = () => {
 	};
 
 	return (
-		<div>
-			<input
-				value={text}
-				key='text'
-				type='text'
-				placeholder='Search by name'
-				onChange={handleChange}
-			/>
-			<button
-				onClick={() => {
-					handleSearch(text);
-				}}>
-				Search
-			</button>
-			<button
-				onClick={() => {
-					handleSearch();
-				}}>
-				All Games
-			</button>
+		<div className={style.searchContainer}>
+			<div className={style.formGroup}>
+				<input
+					className={style.formField}
+					value={text}
+					key='text'
+					type='text'
+					placeholder='Search by name'
+					onChange={handleChange}
+				/>
+				<label for='search by name' className={style.formLabel}>
+					Search by name
+				</label>
+			</div>
+			<div className={style.buttonsContainer}>
+				<button
+					disabled={!text}
+					onClick={() => {
+						handleSearch(text);
+					}}>
+					Search
+				</button>
+				<button
+					onClick={() => {
+						handleSearch();
+					}}>
+					All Games
+				</button>
+			</div>
 		</div>
 	);
 };
